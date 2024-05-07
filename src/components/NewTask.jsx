e9fcceb89a26c8b0e22a5f79ceb31fa900363b2c
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-05-07 20:29:19 +0300

    new

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -25,7 +25,7 @@ const NewTask = ({onAdd }) => {
       />
 
       <button onClick={handleClick} className="text-stone-700 hover:text-slate-950">
-        Add Task
+        Add New Task
       </button>
     </div>
   )
