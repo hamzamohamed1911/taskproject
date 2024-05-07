6f203909d09c99c0c03d19dbc3363b9fc9c75140
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-05-07 20:49:34 +0300

    deployment

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,6 @@
 import { forwardRef , useImperativeHandle ,useRef} from "react";
 import { createPortal } from "react-dom"
-import Button from "./Button";
+import Button from "./Button.jsx";
 
 const Modal =forwardRef(({children , buttonCaption},ref) => {
 
